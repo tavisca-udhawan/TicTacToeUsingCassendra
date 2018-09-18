09b8239f06e9f4126b1e6b1af7d84d595fa40273
Utkarsh Dhawan <udhawan@tavisca.com>
2018-09-18 09:26:52 +0530

    TicTacToe Game using Apache Cassandra

---
+++ TicTacToe Cassendra/TicTacToe/Log.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    public class Log
+    {
+        public string Request { get; set; }
+        public string Response { get; set; }
+        public string Exception { get; set; }
+        public string Time { get; set; }
+    }
+}
---
+++ TicTacToe Cassendra/TicTacToe/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    public class User
+    {
+        public Guid ID { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string userName { get; set; }
+        public string Email { get; set; }
+        public Guid accessToken { get; set; }
+        public string status { get; set; }
+    }
+}
